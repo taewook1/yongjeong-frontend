8f0f348496b1c66a224e8681470c02c645c6e767
taewook1 <aktw96@naver.com>
2025-04-12 20:58:05 +0900

    axios baseURL 중복제거

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + '/api', // 개발에선 프록시로 연결되고, 운영에선 도메인 내 라우트로 연결됨
+  baseURL: import.meta.env.VITE_API_URL, 
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
